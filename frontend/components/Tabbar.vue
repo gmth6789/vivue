3eae8bab32653be07be80693afb0cd46663fe942
Mook.gm99 <143313748+gmth6789@users.noreply.github.com>
2023-09-15 17:01:18 +0000

    main

---
+++ frontend/components/Tabbar.vue
@@ -0,0 +1,66 @@
+
+<template>
+
+<uni-view data-v-27ca9447="" data-v-3f31eb08="" style="--view-theme: #e93323; --view-priceColor: #e93323;
+ --view-minorColor: rgba(233, 51, 35, 0.5);
+--view-minorColorT: rgba(233, 51, 35, 0.1);
+--view-bntColor: #FE960F;">
+<uni-view data-v-27ca9447="">
+<uni-view data-v-27ca9447="" id="target" class="page-footer" style="background-color: rgb(255, 255, 255);">
+<uni-view data-v-27ca9447="" class="foot-item">
+<uni-image data-v-27ca9447="">
+<div style="background-image: url(&quot;https://qiniu.crmeb.net/attach/2023/02/5179b202302161145207007.png&quot;);
+background-position: 0% 0%;
+background-size: 100% 100%;
+background-repeat: no-repeat;">
+</div>
+<!---->
+<img src="https://qiniu.crmeb.net/attach/2023/02/5179b202302161145207007.png" draggable="false">
+</uni-image>
+<uni-view data-v-27ca9447="" class="txt" style="color: rgb(40, 40, 40);">
+<font style="vertical-align: inherit;">
+<font style="vertical-align: inherit;">หน้าแรก</font>
+</font>
+</uni-view>
+</uni-view>
+<uni-view data-v-27ca9447="" class="foot-item">
+<uni-image data-v-27ca9447="">
+<div style="background-image: url(&quot;https://qiniu.crmeb.net/attach/2023/02/0206e202302161145379858.png&quot;); background-position: 0% 0%; background-size: 100% 100%; background-repeat: no-repeat;">
+</div>
+<!---->
+<img src="https://qiniu.crmeb.net/attach/2023/02/0206e202302161145379858.png" draggable="false">
+</uni-image>
+<uni-view data-v-27ca9447="" class="txt" style="color: rgb(40, 40, 40);">
+<font style="vertical-align: inherit;"><font style="vertical-align: inherit;">การจัดหมวดหมู่</font>
+</font>
+</uni-view>
+
+</uni-view>
+
+<uni-view data-v-27ca9447="" class="foot-item">
+<uni-image data-v-27ca9447="">
+<div style="background-image: url(&quot;https://qiniu.crmeb.net/attach/2023/02/fcdd3202302161145516530.png&quot;); background-position: 0% 0%; background-size: 100% 100%; background-repeat: no-repeat;">
+</div>
+<!---->
+<img src="https://qiniu.crmeb.net/attach/2023/02/fcdd3202302161145516530.png" draggable="false">
+</uni-image>
+<uni-view data-v-27ca9447="" class="txt" style="color: rgb(40, 40, 40);">
+<font style="vertical-align: inherit;">
+<font style="vertical-align: inherit;">ตะกร้าสินค้า</font></font></uni-view>
+<!---->
+</uni-view>
+<uni-view data-v-27ca9447="" class="foot-item">
+<uni-image data-v-27ca9447="">
+<div style="background-image: url(&quot;https://qiniu.crmeb.net/attach/2023/02/22fbb202302161146005483.png&quot;); background-position: 0% 0%; background-size: 100% 100%; background-repeat: no-repeat;">
+</div>
+<!---->
+<img src="https://qiniu.crmeb.net/attach/2023/02/22fbb202302161146005483.png" draggable="false">
+</uni-image><uni-view data-v-27ca9447="" class="txt" style="color: rgb(238, 177, 81);">
+<font style="vertical-align: inherit;"><font style="vertical-align: inherit;">ของฉัน</font></font></uni-view><!---->
+
+</uni-view>
+</uni-view>
+</uni-view>
+</uni-view>
+
+</template>
---
+++ frontend/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+
+<div><slot /></div>
+
+
+
+<Tabbar />
+
+</div>
+</template>
