7625390d6a0e1112a304e24ea6abf097256a9c1c
gmth6789 <gmth.6789@gmail.com>
2023-09-16 12:47:22 +0700

    main

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,48 +1,24 @@
-<template>
-  <div class="container">
-     
-
+<van-form @submit="onSubmit">
+  <van-cell-group inset>
+    <van-field
+      v-model="username"
+      name="Username"
+      label="Username"
+      placeholder="Username"
+      :rules="[{ required: true, message: 'Username is required' }]"
+    />
+    <van-field
+      v-model="password"
+      type="password"
+      name="Password"
+      label="Password"
+      placeholder="Password"
+      :rules="[{ required: true, message: 'Password is required' }]"
+    />
+  </van-cell-group>
+  <div style="margin: 16px;">
+    <van-button round block type="primary" native-type="submit">
+      Submit
+    </van-button>
   </div>
-</template>
-
-<script>
-export default {
-  name: 'IndexPage',
-}
-</script>
-
-<style>
-
-
-
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: Quicksand, 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+</van-form>
